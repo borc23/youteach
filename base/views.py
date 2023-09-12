2514d9ddb31c63eb68391b88035eaed773e55220
Bor <borc23@gmail.com>
2023-09-12 17:51:16 +0100

    context to main.html

--- base/views.py
+++ base/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn Python!'},
+    {'id': 2, 'name': 'Lets learn C++!'},
+    {'id': 2, 'name': 'Lets learn JavaScript!'},
+]
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context)
 
 def room(request):
     return render(request, 'room.html')
